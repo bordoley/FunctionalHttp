eafc129f2ae3e4a32a4cbb7da06416bf8b8ac877
Dave <bordoley@gmail.com>
2014-10-24 00:40:13 -0700

    more basic types

--- FunctionalHttp/Core/RequestPreconditions.fs
+++ FunctionalHttp/Core/RequestPreconditions.fs
@@ -1,4 +1,16 @@
 namespace FunctionalHttp
 
-type RequestPreconditions () = 
-    static member None = new RequestPreconditions()
+open System
+
+type AnyTag = |AnyTag
+
+type RequestPreconditions =
+    private {
+        ifMatch: Choice<AnyTag, Set<ETag>> option
+        ifModifiedSince: DateTime option
+        ifNoneMatch: Choice<AnyTag, Set<ETag>> option
+        ifUnmodifiedSince: DateTime option
+        ifRange: Choice<ETag, DateTime> option
+    }
+    
+    static member None = { ifMatch = None; ifModifiedSince = None; ifNoneMatch = None; ifUnmodifiedSince = None; ifRange = None }
---
+++ FunctionalHttp/Data/ETag.fs
@@ -0,0 +1,5 @@
+namespace FunctionalHttp
+
+type ETag =
+    | Weak
+    | Strong
