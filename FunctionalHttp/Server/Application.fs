b5869abe038e56a1661f25395fdcf648eb3abe31
Dave <bordoley@gmail.com>
2015-01-02 08:50:31 -0800

    Stylistic change

--- FunctionalHttp/Server/Application.fs
+++ FunctionalHttp/Server/Application.fs
@@ -8,14 +8,6 @@ type IHttpApplication =
     abstract Filter:  HttpRequest<Stream> -> HttpRequest<Stream>
     abstract Filter: HttpResponse<Stream> -> HttpResponse<Stream>
     abstract Route: HttpRequest<Stream> -> IStreamResource
-
-type internal RoutingApplication (router:Router, defaultResource:IStreamResource) =
-    interface IHttpApplication with
-        member this.Filter (req:HttpRequest<Stream>)= req
-        member this.Filter (resp:HttpResponse<Stream>) = resp
-        member this.Route (req:HttpRequest<Stream>) = 
-            let path = List.ofArray req.Uri.Segments
-            (router.Item path) |> Option.getOrElse defaultResource
      
 module HttpApplication =
     [<CompiledName("SingleResource")>]
@@ -28,7 +20,15 @@ module HttpApplication =
 
     [<CompiledName("Routing")>]
     let routing (resources, defaultResource)  =
-        RoutingApplication ((Router.Empty.AddAll resources), defaultResource) :> IHttpApplication
+        let router = Router.Empty.AddAll resources
+
+        { new IHttpApplication with
+            member this.Filter (req:HttpRequest<Stream>)= req
+            member this.Filter (resp:HttpResponse<Stream>) = resp
+            member this.Route (req:HttpRequest<Stream>) = 
+                let path = List.ofArray req.Uri.Segments
+                (router.Item path) |> Option.getOrElse defaultResource
+        }
 
     [<CompiledName("WithFilters")>]
     let withFilters (requestFilter:RequestFilter<Stream>, responseFilter:ResponseFilter<Stream>)  (application:IHttpApplication) =
