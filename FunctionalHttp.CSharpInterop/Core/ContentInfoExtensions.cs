7c62277c5d564ee4e4603ae0ca112934c7e7ccf0
Dave <bordoley@gmail.com>
2015-01-08 21:12:00 -0800

    add range to With functions.

--- FunctionalHttp.CSharpInterop/Core/ContentInfoExtensions.cs
+++ FunctionalHttp.CSharpInterop/Core/ContentInfoExtensions.cs
@@ -14,7 +14,8 @@ namespace FunctionalHttp.Core.Interop
             IEnumerable<LanguageTag> languages = null,
             UInt64? length = null, 
             Uri location = null, 
-            MediaType mediaType = null)
+            MediaType mediaType = null,
+            FSharpChoice<ByteContentRange, OtherContentRange> range = null)
         {
             return ContentInfoModule.With(
                 encodings.ToFSharpOption(),
@@ -22,6 +23,7 @@ namespace FunctionalHttp.Core.Interop
                 length.ToFSharpOption(),
                 location.ToFSharpOption(),
                 mediaType.ToFSharpOption(),
+                range.ToFSharpOption(),
                 This);
         }
 
@@ -31,9 +33,10 @@ namespace FunctionalHttp.Core.Interop
             bool languages = false,
             bool length = false, 
             bool location = false, 
-            bool mediaType = false)
+            bool mediaType = false,
+            bool range = false)
         {
-            return ContentInfoModule.Without(encodings, languages, length, location, mediaType, This);
+            return ContentInfoModule.Without(encodings, languages, length, location, mediaType, range, This);
         }
     }
 }
