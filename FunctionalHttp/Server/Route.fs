552b459212fa43c0997233ef6839d9a1be30b82d
Dave <bordoley@gmail.com>
2015-02-07 13:59:07 -0800

    Add route/path parsing

--- FunctionalHttp/Server/Route.fs
+++ FunctionalHttp/Server/Route.fs
@@ -26,8 +26,8 @@ type Route =
     override this.ToString() = String.Join("/", this.segments)
 
 [<AutoOpen>]
-module internal RouteExtensions =
-    type private RouteSegment with
+module internal RouteSegmentExtensions =
+    type RouteSegment with
         member this.RouterKey =
             match this with
             | Glob v -> "*"
@@ -92,6 +92,47 @@ module internal Route =
             validate segments Set.empty
             result
         | _ -> failwith "Not a route"
+
+    let getParametersFromPath (route:Route) (path:string list) =
+        let rec getParams (segments: RouteSegment list) (path:string list) (acc:Map<string, string>) =
+            match (segments, path) with
+            | ([],[]) -> acc
+            | ([], _::_) | (_::_, []) -> failwith "path doesn't match the route"
+            | (sHead::sTail, pHead::pTail) -> 
+                match sHead with
+                | Glob v ->
+                    let stopSegment =
+                        match sTail with
+                        | [] -> None
+                        | (Segment next)::_ -> Some next
+                        | _ -> failwith "Invalid route"
+
+                    let rec matchGlobWithStopSegment (path:List<String>) (acc:List<string>) =
+                        match path with
+                        | head::tail when (Some head) = stopSegment -> (path, String.concat "/" acc)
+                        | _::[] | [] -> failwith "path doesn't match the route"
+                        | head::tail ->
+                            let acc = head::acc
+                            matchGlobWithStopSegment tail acc
+
+                    match stopSegment with
+                    | None -> 
+                        acc.Add (v, String.concat "/" path)
+                    | Some seg ->
+                        let (pTail, globMatch) = matchGlobWithStopSegment path []
+                        let acc = acc.Add (v, globMatch)
+                        getParams sTail pTail acc
+
+                | Parameter v ->
+                    let acc = acc |> Map.add v pHead
+                    getParams sTail pTail acc
+                | Segment v when pHead = v -> 
+                    getParams sTail pTail acc
+                | EmptySegment when pHead = "" ->
+                    getParams sTail pTail acc
+                | _ -> failwith "path doesn't match the route"
+
+        getParams (route.ToList()) path Map.empty
 
 type Route with
     static member Create route = Route.create route
